8a19dec05733ecca2d3f48163ee32f51e09693d8
James Grayson <pixelgrass@hotmail.com>
2016-01-02 13:09:04 -0500

    Added message throttling utility class based on Actor

---
+++ Models/MessageThrottle.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace CSharpModels
+{
+	/// <summary>
+	/// To be used when sending bursts of messages at a specified frequency is desired
+	/// Thread Safe - using the actor model
+	/// </summary>
+	/// <typeparam name="T">The message type to enqueue</typeparam>
+	public class MessageThrottle<T> : Actor
+	{
+		private readonly int _messagesPerTick;
+		private readonly int _tickFrequencyMilliseconds;
+		private readonly ITargetBlock<T> _target;
+		private readonly Queue<T> _messageQueue;		
+
+		private Task _messagePump;
+		private bool _isActive;
+
+		public MessageThrottle(ITargetBlock<T> target, int messagePerTick, int tickFrequencyMilliseconds)
+		{
+			_target = target;
+			_messagesPerTick = messagePerTick;
+			_tickFrequencyMilliseconds = tickFrequencyMilliseconds;
+			_messageQueue = new Queue<T>();
+		}
+
+		public void Post(T message)
+		{
+			PerformLight(() => _messageQueue.Enqueue(message));
+		}
+
+		public void Start()
+		{
+			PerformLight(() =>
+			{
+				if (!_isActive)
+				{
+					_isActive = true;
+					_messagePump = MessageTimer();
+				}
+			});
+		}
+
+		private async Task MessageTimer()
+		{
+			while (_isActive)
+			{
+				await SendMessages();
+				var frequency = _tickFrequencyMilliseconds;
+				if (frequency > 0)
+				{
+					await Task.Delay(_tickFrequencyMilliseconds);
+				}
+			}
+		}
+
+		public void Stop()
+		{
+			PerformLight(() =>
+			{
+				_isActive = false;
+				_messagePump.Wait();
+				_messagePump = null;
+			});
+		}
+
+		private Task SendMessages()
+		{
+			return Perform(() =>
+			{
+				if (!_isActive)
+				{
+					return;
+				}
+				if (null != _target)
+				{
+					for (var i = 0; (i < _messagesPerTick) && (_messageQueue.Count > 0); i++)
+					{
+						_target.Post(_messageQueue.Dequeue());
+					}
+				}				
+			});
+		}
+	}
+}
---
+++ ModelsTest/MessageThrottleTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using CSharpModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpModelsTest
+{
+	[TestClass]
+	public class MessageThrottleTest
+	{
+		[TestMethod]
+		public void ThrottleTest()
+		{
+			var count = 0;
+			var actionBlock = new ActionBlock<string>(s =>
+			{
+				count ++;
+			});
+			var messageThrottle = new MessageThrottle<string>(actionBlock,2,100);
+			messageThrottle.Start();
+			for (var i = 0; i < 6; i++)
+			{
+				messageThrottle.Post("test");
+			}
+			Task.Delay(50).Wait(5000);
+			Assert.AreNotEqual(4,count);
+			Task.Delay(100).Wait(5000);
+			Assert.IsTrue(count > 1);
+			Assert.AreNotEqual(6, count);
+			messageThrottle.Stop();
+			Task.Delay(200).Wait(5000);
+			Assert.IsTrue(count > 1);
+			Assert.AreNotEqual(6, count);
+			messageThrottle.Start();
+			Task.Delay(200).Wait(5000);
+			Assert.AreEqual(6, count);
+		}
+	}
+}
